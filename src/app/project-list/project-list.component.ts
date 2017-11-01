fad3561d765690ff2b0e82a2a5ec91816aed6061
Kelsey Langlois and LDMacKrell <kelsey@>
2017-11-01 11:50:51 -0700

    Add project service and display projects on list page.

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
+
+import { Project } from '../project.model';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
-  styleUrls: ['./project-list.component.css']
+  styleUrls: ['./project-list.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectListComponent implements OnInit {
+  projects: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit() {
+    this.projects = this.projectService.getProjects();
   }
 
 }
---
+++ src/app/project.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+import { Project } from './project.model';
+
+@Injectable()
+export class ProjectService {
+  projects: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.projects = database.list('projects');
+  }
+
+  getProjects() {
+    return this.projects;
+  }
+
+}
